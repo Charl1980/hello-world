5b2eca4fc5cee8737349d7322f1d3b8a0d213831
Charl <94119475+Charl1980@users.noreply.github.com>
2022-10-06 11:28:10 +0200

    Updated code with Gifted Chat & components Bubble, Platform, & KeyboardAvoidingView imported

--- components/Chat.js
+++ components/Chat.js
@@ -1,23 +1,81 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
+  }
 
   componentDidMount() {
-    {/* Sets the name typed in the Start screen to be displayed at the top of the Chat screen */ }
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+    {/* Sets the messages state */ }
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "This is a system message",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    {/* Sets the colour of the renderBubble */ }
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+        }}
+      />
+    );
   }
 
   render() {
+    {/* Sets the name typed in the Start screen to be displayed at the top of the Chat screen */ }
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+
     {/* Sets the background color selected on the Start screen */ }
     let color = this.props.route.params.color;
 
     return (
-      <View style={[{ backgroundColor: color }, styles.container]}>
-        <Text style={styles.textColor} >Hello Chat!</Text>
+      <View style={[{ backgroundColor: color }, { flex: 1 }]}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {/* Fixes the issue of the keyboard covering the input field on some Android devices */}
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior='height' /> : null}
       </View>
-    )
+    );
   }
 }
 
